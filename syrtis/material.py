a1603794f877e22a7e5d6edf0a1a79fcafaacd16
= <smross106@gmail.com>
2022-07-18 15:27:18 +0100

    Added StaticShell thermal resistance
    Wrote Material classes for Solid and ConstrainedGas with Rayleigh number calculator. Wrote correlations for vertical and horizontal annular gap heat transfer coefficients into StaticShell

---
+++ syrtis/material.py
@@ -0,0 +1,90 @@
+
+
+from numbers import Number
+import numpy as np
+
+class Material:
+    """
+    Placeholder
+    """
+
+class Solid(Material):
+    """
+    Object for a homogenous solid Shell
+
+    Args:
+        k (float):      Thermal conductivity (W/m/K)
+        rho (float):    Density (kg/m3)
+        cp (float):     Specific heat capacity (kJ/kg/K)
+
+    """
+    def __init__(self, k, rho, cp):
+        assert isinstance(k, Number) and k > 0,   "Material 'k' must be a positive numerical value"
+        assert isinstance(rho, Number) and rho > 0,   "Material 'rho' must be a positive numerical value"
+        assert isinstance(cp, Number) and cp > 0, "Material 'cp' must be a positive numerical value"
+
+        self.k = k
+        self.rho = rho
+        self.cp = cp
+
+class ConstrainedGas(Material):
+    """
+    Object for a constrained gas with no imposed postion (undergoing natural convection)
+
+    Args:
+        T (float):      Temperature (K)
+        p (float):      Pressure (Pa)
+        Pr (float):     Prandtl number
+        mu (float):     Absolute viscosity (Pa s)
+        cp (float):     Isobaric specific heat capacity (kJ/kg/K)
+        k (float):      Thermal conductivity (W/m/K)
+        rho (float):    Density (kg/m3) 
+        beta (float):   Thermal expansion coefficient (1/K) - optional, set to 1/T if left blank
+
+    """
+    def __init__(self, T, p, Pr, mu, cp, k, rho, beta=0.0):
+        assert isinstance(T, Number) and T > 0,   "Material 'T' must be a positive numerical value"
+        assert isinstance(p, Number) and p > 0,   "Material 'p' must be a positive numerical value"
+        assert isinstance(Pr, Number) and Pr > 0, "Material 'Pr' must be a positive numerical value"
+        assert isinstance(mu, Number) and mu > 0, "Material 'mu' must be a positive numerical value"
+        assert isinstance(cp, Number) and cp > 0, "Material 'cp' must be a positive numerical value"
+        assert isinstance(k, Number) and k > 0,   "Material 'k' must be a positive numerical value"
+        assert isinstance(rho, Number) and rho > 0,   "Material 'rho' must be a positive numerical value"
+
+        if beta != 0.0:
+            assert isinstance(beta, Number) and beta > 0,   "Material 'beta' must be a positive numerical value"
+
+        self.T = T
+        self.p = p
+        self.Pr = Pr
+        self.mu = mu
+        self.cp = cp
+        self.k = k
+        self.rho = rho
+
+        if beta == 0.0:
+            self.beta = 1 / self.T
+        else:
+            self.beta = beta
+
+
+    def Ra(self, T_delta, g, length):
+        """
+        Calculate the Rayleigh number, using constant fluid properties
+        """
+        
+        assert isinstance(T_delta, Number), "Input 'T_delta' must be a numerial value"
+        assert isinstance(g, Number) and g > 0,   "Input 'g' must be a positive numerical value"
+        assert isinstance(length, Number) and length > 0,   "Material 'T' must be a positive numerical value"
+
+        Ra = ((g * np.power(length, 3) * self.beta * abs(T_delta) * np.power(self.rho, 2)) 
+        / np.power(self.mu, 2))
+
+        assert Ra > 0, "Input has produced an invalid output"
+
+        return(Ra)
+
+
+
+
+material_classes = [Solid, ConstrainedGas]
--- syrtis/shell.py
+++ syrtis/shell.py
@@ -2,8 +2,16 @@
 Classes and inherited subclasses for Shells, the general term for layers of material included in the simulation.
 In most cases, a Shell will be added as a subclass with more specialised properties
 
+References:
+
+ - [1] - https://en.wikipedia.org/wiki/Thermal_resistance
+ - [2] - Y Cengel, Heat Transfer
+
 """
 
+
+
+from distutils.command.config import config
 
 
 if __name__ == "__main__":
@@ -44,8 +52,92 @@ class StaticShell(Shell):
         assert isinstance(thickness, Number), "Shell 'thickness' must be a numerical value"
         assert thickness > 0, "Shell 'thickness' must be a positive value"
 
+        assert isinstance(length, Number), "Shell 'length' must be a numerical value"
+        assert length > 0, "Shell 'length' must be a positive value"
+
+        self.configuration = configuration
         self.material = material
+
         self.radius_inner = radius_inner
         self.thickness = thickness
         self.radius_outer = self.radius_inner + self.thickness
 
+        self.length = length
+
+    def thermal_resistance(self, T_delta, g):
+        """
+        Calculates the thermal resistance of the Shell
+        Determines which equation to use and uses it
+        """
+
+        if type(self.material) == Solid:
+            self.thermal_resistance = self.thermal_resistance_solid()
+        
+        elif type(self.material) == ConstrainedGas:
+            thickness_ratio = self.thickness / self.radius_inner
+
+            self.thermal_resistance = self.thermal_resistance_wide_annulus(T_delta, g)
+            
+
+    def thermal_resistance_solid(self):
+        # Using equation from Reference [1]
+        R_th = np.log(self.radius_outer / self.radius_inner) / (2 * np.pi * self.material.k * self.length)
+
+        return(R_th)
+
+    def thermal_resistance_wide_annulus(self, T_delta, g):
+        Ra = self.material.Ra(T_delta, g, self.thickness)
+
+        if self.configuration == "horizontal":
+            # Using equation 9-56 from Reference [2] (via Raithby and Hollands, 1975)
+
+            F_cyl = np.power(np.log(self.radius_outer/self.radius_inner), 4) / (
+                np.power(self.thickness, 3) * np.power(
+                    np.power(2 * self.radius_inner, -0.6) + np.power(2 * self.radius_outer, -0.6), 5))
+            # Shape factor of the cylinders
+
+            if F_cyl * Ra > 100:
+                k_eq = (0.386 * np.power(self.material.Pr / (0.861 + self.material.Pr), 0.25) 
+                * np.power(F_cyl * Ra, 0.25))
+                k_eq = max(k_eq, 1)
+            else:
+                k_eq = 1
+            
+        elif self.configuration == "vertical":
+            # Using equation 9-52 to 9-53 from Reference [2] from Berkovsky and Polevikov, 1977
+            ratio = self.length / self.thickness
+
+            if 1 <= ratio and 2 < ratio:
+                k_eq = 0.18 * np.power((self.material.Pr * Ra) / (0.2 + self.material.Pr), 0.29)
+
+                # Ensure model is working in the correct regime
+                if (Ra * self.material.Pr) / (0.2 * self.material.Pr) < 1e3 and k_eq > 1:
+                    print("Warning: heat transfer in vertical enclosure correlation is out of validation range. Proceed with caution")
+                 
+            elif 2 <= ratio < 10:
+                k_eq = 0.22 * np.power((self.material.Pr * Ra) / (0.2 + self.material.Pr), 0.28) * np.power(ratio, -0.25)
+
+                # Ensure model is working in the correct regime
+                if Ra > 1e10 and k_eq > 1:
+                    print("Warning: heat transfer in vertical enclosure correlation is out of validation range. Proceed with caution")
+                
+            elif 10 <= ratio:
+                k_eq = 0.22 * np.power((self.material.Pr * Ra) / (0.2 + self.material.Pr), 0.28) * np.power(ratio, -0.25)
+
+                if k_eq > 1:
+                    print("Warning: heat transfer in vertical enclosure correlation is out of validation range. Proceed with caution")
+            
+            if k_eq < 1:
+                k_eq = 1
+
+        R_th = np.log(self.radius_outer / self.radius_inner) / (2 * np.pi * self.material.k * k_eq * self.length)
+
+        return(R_th)
+
+
+
+co2 = ConstrainedGas(210, 580, 0.71, 10.9e-6, 749, 8.74e-3, 0.0143)
+steel = Solid(150, 8700, 500)
+
+s = StaticShell("horizontal", co2, 1, 0.2, 1)
+s.thermal_resistance(10, 3.8)
